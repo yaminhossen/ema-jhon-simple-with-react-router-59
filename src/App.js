ff19b62ac5acd88475ccedf2fa6e23b4989948e0
yaminhossen <streatdot616@gmail.com>
2022-04-10 11:29:31 +0600

    login form added

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import { Route, Routes } from 'react-router-dom';
 import Orders from './components/Orders/Orders';
 import Inventory from './components/Inventory/Inventory';
 import About from './components/About/About';
+import Login from './components/Login/Login';
+import SignUp from './components/SignUp/SignUp';
 
 function App() {
   return (
@@ -17,6 +19,8 @@ function App() {
         <Route path='/orders' element={<Orders></Orders>}></Route>
         <Route path='/inventory' element={<Inventory></Inventory>}></Route>
         <Route path='/about' element={<About></About>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/signup' element={<SignUp></SignUp>}></Route>
       </Routes>
     </div>
   );
---
+++ src/components/SignUp/SignUp.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './SignUp'
+const SignUp = () => {
+    return (
+        <div>
+            <h3>this is SignUp</h3>
+        </div>
+    );
+};
+
+export default SignUp;
